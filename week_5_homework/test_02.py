8ce46c65bfa6fe54eec63c81618d26e3c4b7d65e
yewintnaing <yewintnaing@jdev.com.mm>
2025-03-07 23:16:46 +0800

    testing for week5 and add homework

---
+++ week_5_homework/test_02.py
@@ -0,0 +1,13 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+df = spark.read \
+    .option("header", "true") \
+    .parquet('fhv_tripdata_2021-01.parquet')
+
+df.show()
